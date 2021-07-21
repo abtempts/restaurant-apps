a1967eb0896503dbc9ab55294f5bc9eb7da94431
Ahmad Riza Abdullah <ahmad.riza.abdullah@gmail.com>
2021-07-21 08:51:21 +0700

    insert sw.js in index.html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -28,17 +28,3 @@ window.addEventListener('DOMContentLoaded', () => {
   swRegister();
   WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
-
-const swRegister = async () => {
-  if (!('serviceWorker' in navigator)) {
-    console.log('Browser tidak mendukung Service Worker');
-    return;
-  }
- 
-  try {
-    await navigator.serviceWorker.register('./sw.js');
-    console.log('Service worker registered');
-  } catch (error) {
-    console.log('Failed to register service worker', error);
-  }
-};
