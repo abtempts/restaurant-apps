4c584057c8b7eeaa4bd79755be23452b817bbc71
Ahmad Riza Abdullah <ahmad.riza.abdullah@gmail.com>
2021-07-21 11:13:09 +0700

    Update sw-register.js

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -3,7 +3,9 @@
 const swRegister = async () => {
   if ('serviceWorker' in navigator) {
     // await runtime.register();
+    console.log('sw-register.js breakpoint1');
     navigator.serviceWorker.register('https://abtempts.github.io/restaurant-apps/dist/sw.js');
+    console.log('sw-register.js breakpoint2');
     return;
   }
   console.log('Service worker not supported in this browser');
