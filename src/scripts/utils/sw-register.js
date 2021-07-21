7bc25c3bddfcad1a148ff64033a008a3ed82a285
Ahmad Riza Abdullah <ahmad.riza.abdullah@gmail.com>
2021-07-21 10:03:08 +0700

    update

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,4 +1,4 @@
-import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+// import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
@@ -7,19 +7,19 @@ const swRegister = async () => {
   }
 
   try {
-    await navigator.serviceWorker.register('./restaurant-apps/sw.js');
+    await navigator.serviceWorker.register('./sw.js');
     console.log('Service worker registered');
   } catch (error) {
     console.log('Failed to register service worker', error);
   }
 
-  if ('serviceWorker' in navigator) {
-    console.log('Breakpoint 1');
-    await runtime.register();
-    console.log('Breakpoint 2');
-    return;
-  }
-  console.log('Service worker not supported in this browser');
+  // if ('serviceWorker' in navigator) {
+  //   console.log('Breakpoint 1');
+  //   await runtime.register();
+  //   console.log('Breakpoint 2');
+  //   return;
+  // }
+  // console.log('Service worker not supported in this browser');
 };
 
 export default swRegister;
