3b3686ae98c4777e8f6df9cd0ce996c9fa8bcc67
Ahmad Riza Abdullah <ahmad.riza.abdullah@gmail.com>
2021-07-21 07:50:30 +0700

    Update sw-register.js

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -2,7 +2,9 @@ import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
 const swRegister = async () => {
   if ('serviceWorker' in navigator) {
+    console.log('Breakpoint 1');
     await runtime.register();
+    console.log('Breakpoint 2');
     return;
   }
   console.log('Service worker not supported in this browser');
