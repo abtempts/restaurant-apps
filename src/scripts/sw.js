6f2eb0bc4003c64fedb2396d0033585fb5224dc1
Ahmad Riza Abdullah <ahmad.riza.abdullah@gmail.com>
2021-07-21 13:27:35 +0700

    Update sw.js

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -5,7 +5,7 @@ import CacheHelper from './utils/cache-helper';
 const { assets } = global.serviceWorkerOption;
 
 try {
-  navigator.serviceWorker.register('./restaurant-apps/sw.js');
+  navigator.serviceWorker.register('/restaurant-apps/dist/sw.js');
   console.log('Service worker registered');
 } catch (error) {
   console.log('Failed to register service worker', error);
