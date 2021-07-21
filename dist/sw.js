189dacf2193ea7061f65154d7a6de3d546633ba3
Ahmad Riza Abdullah <ahmad.riza.abdullah@gmail.com>
2021-07-21 15:20:59 +0700

    Update sw.js

--- dist/sw.js
+++ dist/sw.js
@@ -1,18 +1,18 @@
 var serviceWorkerOption = {
   "assets": [
-    "/defaultVendors~main~678f84af.bundle.js",
-    "/defaultVendors~main~d939e436.bundle.js",
-    "/main~29d6ecf2.bundle.js",
-    "/manifest.json",
-    "/icons/ico-128.png",
-    "/icons/ico-256.png",
-    "/icons/ico-32.png",
-    "/icons/ico-48.png",
-    "/icons/ico-512.png",
-    "/icons/ico-64.png",
-    "/icons/ico-72.png",
-    "/icons/ico-96.png",
-    "/index.html"
+    '/dist/defaultVendors~main~678f84af.bundle.js',
+  '/dist/defaultVendors~main~d939e436.bundle.js',
+  '/dist/main~29d6ecf2.bundle.js',
+  '/dist/manifest.json',
+  '/dist/icons/ico-128.png',
+  '/dist/icons/ico-256.png',
+  '/dist/icons/ico-32.png',
+  '/dist/icons/ico-48.png',
+  '/dist/icons/ico-512.png',
+  '/dist/icons/ico-64.png',
+  '/dist/icons/ico-72.png',
+  '/dist/icons/ico-96.png',
+  '/index.html',
   ]
 };
         
